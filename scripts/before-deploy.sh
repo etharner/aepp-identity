0c21edb4bd8ecbb645d969dc38959ce3632c1a16
etharner <etharner@gmail.com>
2018-09-26 18:02:10 +1000

    Add homebrew caching in travis build

--- scripts/before-deploy.sh
+++ scripts/before-deploy.sh
@@ -5,9 +5,9 @@ if [[ $TRAVIS_OS_NAME != "osx" ]]; then
   npm run build
 else
   brew install imagemagick
-  npm run build:cordova
-  npx cordova prepare ios
-  npm run gen:cordova-resources
-  npx cordova build ios
-  tar -czvf aeternity.app.tar.gz -C platforms/ios/build/emulator aeternity.app
+  # npm run build:cordova
+  # npx cordova prepare ios
+  # npm run gen:cordova-resources
+  # npx cordova build ios
+  # tar -czvf aeternity.app.tar.gz -C platforms/ios/build/emulator aeternity.app
 fi
